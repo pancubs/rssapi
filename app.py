e221004ceb3c7634e11cd0f2a291a4f47589a380
Sonny Kothapally <me@sonnyksimon.com>
2019-11-29 15:16:59 -0400

    working

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 import flask
 import feedparser
+import pytz
+import datetime
+import time
 
-def get_posts_by_subscription(s, _start):
+def get_posts_by_subscription(s, _start=False):
     utc = pytz.utc
     if not _start:
         # I want only posts from "today,"
         # where the day lasts until 2 AM.
         homeTZ = pytz.timezone('US/Arizona')
-        dt = datetime.now(homeTZ)
+        dt = datetime.datetime.now(homeTZ)
         if dt.hour < 2:
             dt = dt - timedelta(hours=24)
         start = dt.replace(hour=0, minute=0, second=0, microsecond=0)
         start = start.astimezone(utc)
     else:
-        start = _start.replace(hour=0, minute=0, second=0, microsecond=0)
+        start = datetime.datetime.strptime(_start, "%Y-%m-%d").replace(hour=0, minute=0, second=0, microsecond=0)
         start = start.astimezone(utc)
     _posts = []
-    f = fp.parse(s)
+    f = feedparser.parse(s)
     try:
         blog = f['feed']['title']
     except KeyError:
@@ -26,7 +29,7 @@ def get_posts_by_subscription(s, _start):
             when = e['published_parsed']
         except KeyError:
             when = e['updated_parsed']
-        when = utc.localize(datetime.fromtimestamp(time.mktime(when)))
+        when = utc.localize(datetime.datetime.fromtimestamp(time.mktime(when)))
         if when > start:
             title = e['title']
             try:
@@ -39,14 +42,29 @@ def get_posts_by_subscription(s, _start):
 
 app = flask.Flask(__name__)
 
-@app.route('/posts', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def posts():
     if flask.request.method == 'POST':
-        i = flask.request.form
+        i = flask.request.get_json()
         if i.get('start', False):
             return flask.jsonify(data=get_posts_by_subscription(i['subscription'], i['start']))
         else:
             return flask.jsonify(data=get_posts_by_subscription(i['subscription']))
+    else:
+        resp = flask.Response("""
+Usage
+~~~~~
+
+curl -X POST \\
+  http://localhost:5000/posts \\
+  -H 'Content-Type: application/json' \\
+  -d '{
+    "subscription": "https://www.stabroeknews.com/feed",
+    "start": "2019-11-29"
+}'
+""")
+        resp.headers['content-type'] = 'text/plain'
+        return resp
 
 if __name__ == '__main__':
     app.run()
